0ade4346cb6ef13cc1920d7971fd594613124816
Edward <hua9939@gmail.com>
2023-12-09 00:18:19 +1300

    add description on the new columns

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ with col1:
 with col2:
     st.title("Edward")
     content = """
-    Hi, My name is Edward. I graduated from university of Canterbury with Bachelor's degree in computer science 
+    Hi, My name is Edward. I graduated from University of Canterbury with Bachelor's degree in computer science 
      and I am currently studying a master degree in Applied Computing. I am trying to enhance my skills and do more 
      practice through the course from Udemy. I am learning the Web development via this project. 
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps that I have learned from the course and built in Python. 
+         Feel free to contact me if you see my effect."""
+st.write(content2)
